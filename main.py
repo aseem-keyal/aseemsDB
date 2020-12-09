66c9056ca0acd44066d4540c8585c90b6e331e32
Aseem Keyal <aseem.keyal@gmail.com>
2020-12-09 14:13:55 -0800

    basic search

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from typing import Optional
 from recoll import recoll
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/results")
-def read_item(query: str,
+# API methods
+@app.get("/results", response_class=HTMLResponse)
+async def results(request: Request, 
+        query: str,
         searchType: Optional[int] = 1,
         dir: Optional[str] = "<all>",
         sort: Optional[str] = "url",
         ascending: Optional[int] = 0,
         page: Optional[int] = 1):
+    results = recoll_query(query, searchType, dir, sort, ascending, page)
+    return templates.TemplateResponse("results.html", {"request": request, "results": results})
+
+@app.get("/query")
+async def run_query(query: str,
+        searchType: Optional[int] = 1,
+        dir: Optional[str] = "<all>",
+        sort: Optional[str] = "url",
+        ascending: Optional[int] = 0,
+        page: Optional[int] = 1):
+    return recoll_query(query, searchType, dir, sort, ascending, page)
+
+# Helper methods
+def recoll_query(query, searchType, dir, sort, ascending, page):
     db = recoll.connect()
     q = db.query()
     nres = q.execute(query)
     results = q.fetchmany(20)
     return results
+
